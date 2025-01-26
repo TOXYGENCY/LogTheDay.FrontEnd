e457960528692f8c0671d8bcc94531abc83ac4f7
TOXYGENCY <mailforsomethingelse@gmail.com>
2025-01-26 12:34:24 +0300

    начат переход на Angular v19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'auth', component: RegistrationPageComponent },
+    { path: '', component: RegistrationPageComponent },
+    { path: 'main', component: MainPageComponent }, // Страница глав
+    { path: '**', redirectTo: 'auth' } // Перенаправление на логин для несуществующих маршрутов
+];
